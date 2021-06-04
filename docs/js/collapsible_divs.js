de0ad0ff4c1815b0d88c3f47a0d8a2d91665d77b
Elliott Fogg <elliott.fogg@gmail.com>
2021-06-04 17:32:39 +0100

    Refactored Collapsible Divs to use the much simpler Details element. No CSS yet.

--- docs/js/collapsible_divs.js
+++ docs/js/collapsible_divs.js
@@ -1,42 +1,74 @@
-function create_collapsible_div(title, content, class_name=null) {
-    var container = document.createElement("div");
-    container.classList.add("collapsible-div-container");
-    if (class_name != null) {
-        container.classList.add(...class_name.split(" "));
-    }
+function create_collapsible_div(title_content, body_content, class_name=null) {
+    var details = document.createElement("details");
+    details.className = class_name;
 
-    // Create the clickable Title
-    var title_div = document.createElement("div");
-    title_div.classList.add("collapsible-div-title");
-    title_div.onclick = collapsible_div_title_onclick;
-    if (typeof title === "string") {
-        var title_p = document.createElement("p");
-        title_p.innerHTML = title;
-        title_div.appendChild(title_p);
-    } else if (Array.isArray(title)) {
-        for (let elem of title) {
-            title_div.appendChild(elem);
+    // Create the title
+    var title = document.createElement("summary");
+    if (typeof title_content === "string") {
+        title.innerHTML = title_content;
+    } else if (Array.isArray(title_content)) {
+        for (let elem of title_content) {
+            title.appendChild(elem);
         }
     } else {
-        title_div.appendChild(title);
+        title_div.appendChild(title_content);
     }
 
-    var content_div = document.createElement("div");
-    content_div.classList.add("collapsible-div-content");
-    if (typeof content == "string") {
-        let content_p = document.createElement("p");
-        content_p.innerHTML = content;
-        content_div.appendChild(content_p);
-    } else if (typeof content == "object") {
-        for (let elem of content) {
-            content_div.appendChild(elem);
+    var content = document.createElement("div");
+    if (typeof body_content === "string") {
+        content.innerHTML = body_content;
+    } else if (Array.isArray(body_content)) {
+        for (let elem of body_content) {
+            content.appendChild(elem);
         }
+    } else {
+        content.appendChild(body_content);
     }
 
-    container.appendChild(title_div);
-    container.appendChild(content_div);
-    return container;
+    details.appendChild(title);
+    details.appendChild(content);
+    return details;
 }
+
+// function create_collapsible_div(title, content, class_name=null) {
+//     var container = document.createElement("div");
+//     container.classList.add("collapsible-div-container");
+//     if (class_name != null) {
+//         container.classList.add(...class_name.split(" "));
+//     }
+
+//     // Create the clickable Title
+//     var title_div = document.createElement("div");
+//     title_div.classList.add("collapsible-div-title");
+//     title_div.onclick = collapsible_div_title_onclick;
+//     if (typeof title === "string") {
+//         var title_p = document.createElement("p");
+//         title_p.innerHTML = title;
+//         title_div.appendChild(title_p);
+//     } else if (Array.isArray(title)) {
+//         for (let elem of title) {
+//             title_div.appendChild(elem);
+//         }
+//     } else {
+//         title_div.appendChild(title);
+//     }
+
+//     var content_div = document.createElement("div");
+//     content_div.classList.add("collapsible-div-content");
+//     if (typeof content == "string") {
+//         let content_p = document.createElement("p");
+//         content_p.innerHTML = content;
+//         content_div.appendChild(content_p);
+//     } else if (typeof content == "object") {
+//         for (let elem of content) {
+//             content_div.appendChild(elem);
+//         }
+//     }
+
+//     container.appendChild(title_div);
+//     container.appendChild(content_div);
+//     return container;
+// }
 
 function collapsible_div_title_onclick(event) {
     // console.log(event);
