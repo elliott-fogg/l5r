887e86171f09bd43d0d0c83e44daea5b4fdb455a
Elliott Fogg <elliott.fogg@gmail.com>
2021-11-16 23:13:16 +0000

    Testing sending variables to the worker

--- docs/js/dice_roller_worker.js
+++ docs/js/dice_roller_worker.js
@@ -1,4 +1,10 @@
-onmessage = function(e) {
-	postMessage("WORKER STARTED!");
+self.addEventListener("message", function(e) {
+	var data = e.data;
+	var roll = data.roll;
+	var keep = data.keep;
+	var explode_range = data.explode_range;
+	var emphasis = data.emphasis;
+
+	postMessage(`Worker Started! Roll: ${roll}, Keep: ${keep}, X: ${explode_range}, Emphasis: ${emphasis}`);
 	self.close();
-}
+});
